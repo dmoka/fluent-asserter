4bf42a09c772fd21457b63af5f5d92a2507fa840
dmoka <mokadaniel89@gmail.com>
2022-01-03 18:28:45 +0100

    Add checker and is_equal_to method implementation with dummy implementation

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,36 @@
+pub struct Checker<T> {
+    value : T
+}
+
+impl Checker<String> {
+
+    pub fn new(value: String) -> Self {
+        Checker {
+            value
+        }
+    }
+
+    pub fn is_equal_to(self, expected_value: &str) {
+        println!("hello from is is equal method");
+    }
+}
+
+#[macro_export]
+macro_rules! assert_that {
+    ($value:expr) => {
+        Checker::new($value)
+    };
+}
+
+
 #[cfg(test)]
 mod tests {
+    use super::*;
+
     #[test]
     fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
+        let checker = assert_that!(String::from("dani"));
+
+        checker.is_equal_to("s")
     }
 }
