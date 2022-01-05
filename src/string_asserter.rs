c0c20122b4e062792874f93128e3e32c4963a12c
dmoka <mokadaniel89@gmail.com>
2022-01-05 20:47:07 +0100

    add starts and ends with assertions

--- src/string_asserter.rs
+++ src/string_asserter.rs
@@ -1,5 +1,7 @@
 use super::*;
 
+//TODO: add and
+
 impl Asserter<&str> {
 
     pub fn new(value: &str) -> Asserter<&str> {
@@ -20,7 +22,24 @@ impl Asserter<&str> {
         }
     }
 
-    //TODO: startsWith, endsWith, isEmpty, isNothEmpty, has length
+    pub fn starts_with(self, expected_start: &str) {
+        let starts_with = self.value.starts_with(expected_start);
+
+        if !starts_with {
+            panic!("The actual text {} does not start with {}", self.value, expected_start)
+        }
+    }
+
+    pub fn ends_with(self, expected_start: &str) {
+        let starts_with = self.value.ends_with(expected_start);
+
+        if !starts_with {
+            panic!("The actual text {} does not end with {}", self.value, expected_start)
+        }
+
+    }
+
+    //TODO: isEmpty, isNothEmpty, has length
 }
 
 #[cfg(test)]
@@ -32,32 +51,50 @@ mod test {
 
     #[test]
     fn test_is_equal_to_for_string() {
-        let text = String::from("test string");
+        assert_that!(&String::from("test string")).is_equal_to("test string");
 
-        assert_that!(&text).is_equal_to("test string");
+        assert_that!(&String::from("bitcoin")).is_equal_to("bitcoin");
     }
 
 
     #[test]
     fn test_is_equal_to_for_str() {
-        let text = "test string";
-
-        assert_that!(text).is_equal_to("test string");
+        assert_that!("test string").is_equal_to("test string");
+        
+        assert_that!("bitcoin").is_equal_to("bitcoin");
     }
 
     #[test]
     fn when_string_contains_string_then_no_error() {
-        let text = String::from("test string");
+        assert_that!(&String::from("test string")).contains("st");
 
-        assert_that!(&text).contains("st");
+        assert_that!(&String::from("test string")).contains("ing");
     }
 
 
     #[test]
-    fn when_string_does_contains_string_then_panics() {
-        let text = String::from("test string");
+    fn when_string_does_not_contain_string_then_panics() {
+        assert_that_panics(|| assert_that!(&String::from("test string")).contains("asd"));
 
-        assert_that_panics(|| assert_that!(&text).contains("asd"));
+        assert_that_panics(|| assert_that!(&String::from("bitcoin")).contains("ethereum"));
+    }
+
+    #[test]
+    fn test_string_starts_with() {
+        assert_that!(&String::from("test string")).starts_with("te");
+        assert_that!(&String::from("bitcoin")).starts_with("bitcoin");
+
+        assert_that_panics(|| assert_that!(&String::from("test string")).contains("asd"));
+        assert_that_panics(|| assert_that!(&String::from("bitcoin")).contains("eth"));
+    }
+
+    #[test]
+    fn test_string_ends_with() {
+        assert_that!(&String::from("test string")).ends_with("ng");
+        assert_that!(&String::from("bitcoin")).ends_with("coin");
+
+        assert_that_panics(|| assert_that!(&String::from("test string")).ends_with("asd"));
+        assert_that_panics(|| assert_that!(&String::from("bitcoin")).ends_with("eth"));
     }
 
     //TODO: add different assertion message? check asserteq
