70f47885743956c6b1c796c50391a93d1d8ddcd6
dmoka <mokadaniel89@gmail.com>
2022-01-12 20:27:48 +0100

    add is_smaller_than_or_equal_to asserter method

--- src/number_asserter.rs
+++ src/number_asserter.rs
@@ -2,6 +2,12 @@ use super::*;
 
 impl<T> Asserter<T> where T : Copy + PartialOrd + std::fmt::Debug + std::fmt::Display {
     pub fn is_smaller_than(self, expected: T) {
+        if self.value >= expected {
+            panic!("The value {} is not smaller than {}", self.value, expected)
+        }
+    }
+
+    pub fn is_smaller_than_or_equal_to(self, expected: T) {
         if self.value > expected {
             panic!("The value {} is not smaller than {}", self.value, expected)
         }
@@ -14,12 +20,20 @@ mod test {
     use super::*;
     use crate::panic_asserter_helper::assert_that_panics;
 
-    //TODO: We should add these tests to lib.rs
     #[test]
     fn test_is_smaller_than() { 
         assert_that!(3).is_smaller_than(4);
         assert_that!(21.0).is_smaller_than(21.1);
 
         assert_that_panics(||assert_that!(5).is_smaller_than(4));
+        assert_that_panics(||assert_that!(10).is_smaller_than(10));
+    }
+
+    #[test]
+    fn test_is_smaller_than_or_equal_to() { 
+        assert_that!(4).is_smaller_than_or_equal_to(4);
+        assert_that!(21.0).is_smaller_than_or_equal_to(21.1);
+
+        assert_that_panics(||assert_that!(4.01).is_smaller_than_or_equal_to(4.0));
     }
 }
