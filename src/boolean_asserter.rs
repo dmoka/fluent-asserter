601c78ce8b15004b7bbdaea46dd40ffa19170e01
dmoka <mokadaniel89@gmail.com>
2022-01-25 20:43:30 +0100

    add boolean asserter

---
+++ src/boolean_asserter.rs
@@ -0,0 +1,49 @@
+use super::*;
+
+impl Asserter<bool> {
+    pub fn is_true(&self) 
+    {
+        if self.value != true {
+            panic!("AssertionFailure");//TODO: use proper error message
+        }
+    }
+
+    pub fn is_false(&self) 
+    {
+        if self.value != false {
+            panic!("AssertionFailure");//TODO: use proper error message
+        }
+    }
+}
+
+//TODO: S - add this to tests folder
+#[cfg(test)]
+mod test {
+    use super::*;
+
+
+    #[test]
+    fn test_is_true() {
+        assert_that!(true).is_true();
+
+        assert_that_code!(||assert_that!(false).is_true()).panics();
+    }
+
+    #[test]
+    fn test_is_false() {
+        assert_that!(false).is_false();
+
+        assert_that_code!(||assert_that!(true).is_false()).panics();
+    }
+
+    
+    #[test]
+    fn test_is_equal_to() {
+        assert_that!(false).is_equal_to(false);
+        assert_that!(true).is_equal_to(true);
+
+        assert_that_code!(||assert_that!(true).is_equal_to(false)).panics();
+        assert_that_code!(||assert_that!(false).is_equal_to(true)).panics();
+    }
+
+}
--- src/lib.rs
+++ src/lib.rs
@@ -9,6 +9,7 @@ mod panic_asserter;
 mod number_asserter;
 mod number_approx_asserter;
 mod assertion_failure_message;
+mod boolean_asserter;
 
 use std::{panic};
 use std::borrow::Borrow;
