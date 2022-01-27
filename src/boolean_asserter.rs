86ea3be69e06d91609338f9a0c9e5b9d05b7d83f
dmoka <mokadaniel89@gmail.com>
2022-01-27 18:07:27 +0100

    organizing tests

--- src/boolean_asserter.rs
+++ src/boolean_asserter.rs
@@ -29,19 +29,6 @@ mod test {
     }
 
     #[test]
-    fn test_error_message () {
-        let false_var = false;
-        assert_that_code!(||assert_that!(false_var).is_true())
-            .panics()
-            .with_message("Expected false_var to be true, but was false");
-
-        let true_var = true;
-        assert_that_code!(||assert_that!(true_var).is_false())
-            .panics()
-            .with_message("Expected true_var to be false, but was true");
-    }
-
-    #[test]
     fn test_is_false() {
         assert_that!(false).is_false();
 
@@ -57,6 +44,19 @@ mod test {
 
         assert_that_code!(||assert_that!(true).is_equal_to(false)).panics();
         assert_that_code!(||assert_that!(false).is_equal_to(true)).panics();
+    }
+
+    #[test]
+    fn test_error_message () {
+        let false_var = false;
+        assert_that_code!(||assert_that!(false_var).is_true())
+            .panics()
+            .with_message("Expected false_var to be true, but was false");
+
+        let true_var = true;
+        assert_that_code!(||assert_that!(true_var).is_false())
+            .panics()
+            .with_message("Expected true_var to be false, but was true");
     }
 
 }
