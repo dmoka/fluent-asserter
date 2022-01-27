d4d7dd7c327d0474110409959468b8aa035abcc5
dmoka <mokadaniel89@gmail.com>
2022-01-27 18:04:52 +0100

    removed done todo comments

--- src/boolean_asserter.rs
+++ src/boolean_asserter.rs
@@ -4,14 +4,14 @@ impl Asserter<bool> {
     pub fn is_true(&self) 
     {
         if !self.value {
-            panic!("Expected {} to be true, but was false", self.name);//TODO: use proper error message
+            panic!("Expected {} to be true, but was false", self.name);
         }
     }
 
     pub fn is_false(&self) 
     {
         if self.value {
-            panic!("Expected {} to be false, but was true", self.name);//TODO: use proper error message
+            panic!("Expected {} to be false, but was true", self.name);
         }
     }
 }
