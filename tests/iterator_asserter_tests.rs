085e3206571c9e48b647182226f75111df0cb235
dmoka <mokadaniel89@gmail.com>
2022-02-04 19:56:14 +0100

    add assetion for panic message

--- tests/iterator_asserter_tests.rs
+++ tests/iterator_asserter_tests.rs
@@ -6,6 +6,11 @@ mod test_number_asserter {
 
     #[test]
     fn test_is_equal_to() { 
-        assert_that!(vec!["s"]).is_equal_to(vec!["s"])
+        assert_that!(vec!["item1"]).is_equal_to(vec!["item1"]);
+
+        let list = vec!["item1"];
+        assert_that_code!(||assert_that!(list).is_equal_to(vec!["item2"]))
+            .panics()
+            .with_message("Expected list to be [\"item2\"], but was [\"item1\"]")
     }
 }
