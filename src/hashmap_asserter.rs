0f1abb7a5841cb9481bde199682332f30b545663
dmoka <mokadaniel89@gmail.com>
2022-02-17 20:11:44 +0100

    Add does not contain key assertion

--- src/hashmap_asserter.rs
+++ src/hashmap_asserter.rs
@@ -3,12 +3,16 @@ use std::collections::HashMap;
 use std::fmt::Display;
 use std::hash::Hash;
 
-//TODO: finish it
-//TODO: do it for non reference type too?
 impl<K,V> Asserter<&HashMap<K,V>> where K : Eq + Hash + Display {
     pub fn contains_key(&self, expected_key: K) {
         if !&self.value.contains_key(&expected_key) {
             panic!("Expected {} to contain {}, but it does not.",&self.name, &expected_key);
+        }
+    }
+
+    pub fn does_not_contain_key(&self, not_expected_key: K) {
+        if *&self.value.contains_key(&not_expected_key) {
+            panic!("Expected {} to not to contain {}, but it does.",&self.name, &not_expected_key);
         }
     }
 }
@@ -28,6 +32,19 @@ mod test_hashmap_asserter {
         assert_that_code!(||assert_that!(&hash_map).contains_key(String::from("key2")))
             .panics()
             .with_message("Expected &hash_map to contain key2, but it does not.");
+    }
+
+    #[test]
+    fn test_does_not_contain_key() {
+        let mut hash_map = HashMap::<String,String>::new();
+        hash_map.insert(String::from("key"),String::from("value"));
+
+        assert_that!(&hash_map).does_not_contain_key(String::from("key2"));
+
+        assert_that_code!(||assert_that!(&hash_map).does_not_contain_key(String::from("key")))
+            .panics()
+            .with_message("Expected &hash_map to not to contain key, but it does.");
+
     }
 
     //TODO: normal iterator assertions works with hashmap. Somehow limit it.
