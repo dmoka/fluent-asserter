a5a00fd213ea3f58b7b51484b08b57da7b8892c0
dmoka <mokadaniel89@gmail.com>
2022-02-17 20:03:16 +0100

    Add contains key method

--- src/hashmap_asserter.rs
+++ src/hashmap_asserter.rs
@@ -1,33 +1,33 @@
 use super::*;
 use std::collections::HashMap;
+use std::fmt::Display;
+use std::hash::Hash;
 
 //TODO: finish it
 //TODO: do it for non reference type too?
-impl<K,V> Asserter<&HashMap<K,V>>  {
-    pub fn contains_key(&self, expected_key: String) {
-        /*if (self.value).contains_key(&expected_key) {
-
-        }*/
+impl<K,V> Asserter<&HashMap<K,V>> where K : Eq + Hash + Display {
+    pub fn contains_key(&self, expected_key: K) {
+        if !&self.value.contains_key(&expected_key) {
+            panic!("Expected {} to contain {}, but it does not.",&self.name, &expected_key);
+        }
     }
 }
 
 //TODO: S - add this to tests folder
 #[cfg(test)]
-mod test {
+mod test_hashmap_asserter {
     use super::*;
 
     #[test]
-    #[ignore]
-    fn test_is_true() {
+    fn test_contains_key() {
         let mut hash_map = HashMap::<String,String>::new();
         hash_map.insert(String::from("key"),String::from("value"));
 
         assert_that!(&hash_map).contains_key(String::from("key"));
 
         assert_that_code!(||assert_that!(&hash_map).contains_key(String::from("key2")))
-            .panics();
-
-
+            .panics()
+            .with_message("Expected &hash_map to contain key2, but it does not.");
     }
 
     //TODO: normal iterator assertions works with hashmap. Somehow limit it.
