08cac7ef97bca81d961dad300864d8be65a8e8ad
dmoka <mokadaniel89@gmail.com>
2022-02-23 18:14:01 +0100

    Add comment to boolean assertions

--- src/boolean_asserter.rs
+++ src/boolean_asserter.rs
@@ -2,12 +2,14 @@ use super::*;
 
 //TODO: make trait and implement it.
 impl Asserter<bool> {
+    /// Checks if the boolean is true.
     pub fn is_true(&self) {
         if !self.value {
             panic!("Expected {} to be true, but was false", self.name);
         }
     }
 
+    /// Checks if the boolean is true.
     pub fn is_false(&self) {
         if self.value {
             panic!("Expected {} to be false, but was true", self.name);
