483be717c565ae7583496058b9d9e9f22e2bd14d
dmoka <mokadaniel89@gmail.com>
2022-02-23 18:12:45 +0100

    Format boolean asserter file

--- src/boolean_asserter.rs
+++ src/boolean_asserter.rs
@@ -1,19 +1,16 @@
 use super::*;
 
-//TODO: make trait and implement it
+//TODO: make trait and implement it.
 impl Asserter<bool> {
-    pub fn is_true(&self) 
-    {
+    pub fn is_true(&self) {
         if !self.value {
             panic!("Expected {} to be true, but was false", self.name);
         }
     }
 
-    pub fn is_false(&self) 
-    {
+    pub fn is_false(&self) {
         if self.value {
             panic!("Expected {} to be false, but was true", self.name);
         }
     }
 }
-
